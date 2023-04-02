bf6ead3be83c1cf30081ca4abb8ca3d0c94165ae
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 13:58:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ bot = interactions.Client(
     default_scope=Globals.SERVER_ID,
     )
 
-
+# 登录状态确认
 @bot.event
 async def on_ready():
     print(f"Logged in Alright")
 
+
+
+# 测试用的狗屎代码
 @bot.command(
     name = "fuck",
     description = "重构一坨狗屎放在这里",
@@ -23,6 +26,27 @@ async def my_first_command(ctx: interactions.CommandContext, propmt: str = "SHIT
     await ctx.send("Fucking Good Guys about This {}".format(propmt))
 
 
+
+# 调用imagine
+@bot.command(
+    name = "dj",
+    description = "问问远处的机器人吧家人们",
+)
+@interactions.option()
+async def mj_imagine(ctx, prompt: str):
+
+    if (Globals.USE_MESSAGED_CHANNEL):
+        Globals.CHANNEL_ID = ctx.channel.id
+
+    response = PassPromptToSelfBot(prompt)
+    
+    if response.status_code >= 400:
+        print(response.txt)
+        print(response.status_code)
+        await ctx.respond("Request has failed; please try later")
+    else:
+        await ctx.respond(
+            "Your image is being prepared, please wait a moment...")
 
 
 bot.start()
