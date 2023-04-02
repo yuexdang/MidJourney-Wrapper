9222c4db5ca852dec4f35d9f3e9f0d47925bbb44
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 12:50:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ from discord.ext import commands
 import Globals
 from Salai import PassPromptToSelfBot, Upscale, MaxUpscale, Variation
 
-intents = discord.Intents.all()
+# intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="/",intents=intents)
+bot = commands.Bot(
+    command_prefix="/",
+#     intents=intents
+)
 
-
-class MyHelpCommand(commands.DefaultHelpCommand):
-    async def command_not_found(self, string):
-        return f"没有找到名为'{string}'的命令。"
-
-    async def command_has_no_subcommands(self, command):
-        return f"{command}没有子命令。"
 
 
 
@@ -24,6 +20,6 @@ async def example(ctx):
 
 
 
-bot.help_command = MyHelpCommand()
+bot.help_command = commands.DefaultHelpCommand(no_category='Commands')
 
 bot.run(Globals.DAVINCI_TOKEN)
