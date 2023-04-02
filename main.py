0ced147f288345e3b816f97a3d9bf3c3d883611f
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 14:14:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ async def on_ready():
 # 检测at了哪个图图
 @bot.event
 async def on_message(message):
+    print(message)
     if message.content == "": return
     if "!15dj" in message.content and message.content[0] == '!':
         try:
