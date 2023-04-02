ebeeed1a2f8e9034cca5641ec100b2ca841b5f22
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 13:31:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = interactions.Client(token=os.environ.get('DAVINCI_TOKEN'))
 @bot.command(
     name="my_first_command",
     description="This is the first command I made!",
-    scope=os.environ.get('CHANNEL_ID'),
+    scope=os.environ.get('SERVER_ID'),
 )
 async def my_first_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
