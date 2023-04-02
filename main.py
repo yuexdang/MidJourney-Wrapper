fda3f713f8120b5652d83db455f4dc5c78bb2284
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 13:45:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}")
 
 @bot.command(
-    name = "Fuck",
-    description = "Holy shit It Works",
+    name = "fuck",
+    description = "重构一坨狗屎放在这里",
     scope = Globals.SERVER_ID,
 )
 async def my_first_command(ctx: interactions.CommandContext, propmt: str = "SHIT CODE"):
