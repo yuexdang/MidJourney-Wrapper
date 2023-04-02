3e449f27770e1569a74309e6fbf3f389c569cd27
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 13:53:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import Globals
 import interactions
 
 
-bot = interactions.Client(token=Globals.DAVINCI_TOKEN)
+bot = interactions.Client(
+    token=Globals.DAVINCI_TOKEN，
+    default_scope=Globals.SERVER_ID,
+    )
 
 
 @bot.event
@@ -14,9 +17,12 @@ async def on_ready():
 @bot.command(
     name = "fuck",
     description = "重构一坨狗屎放在这里",
-    scope = Globals.SERVER_ID,
 )
+@interactions.option()
 async def my_first_command(ctx: interactions.CommandContext, propmt: str = "SHIT CODE"):
     await ctx.send("Fucking Good Guys about This {}".format(propmt))
+
+
+
 
 bot.start()
