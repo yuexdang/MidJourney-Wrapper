2f9ff2a0796d9511ea968a1ffb1ee2a462e6661b
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-02 13:20:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import interactions, os
 bot = interactions.Client(token=os.environ.get('DAVINCI_TOKEN'))
 
 @bot.command(
-    name="my_first_command",
+    name="my_first_command yeap",
     description="This is the first command I made!",
     scope=the_id_of_your_guild,
 )
