9989e8c6de76adf83a4951c23562434f65aac559
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-03 14:01:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ async def mj_imagine(ctx, prompt: str):
             name="change_sign",
             description="选择细分类型",
             type=interactions.OptionType.STRING,
-            required=False,
+            required=True,
             choices = [
                 interactions.Choice(name="U:继承细分", value="U"),
                 interactions.Choice(name="V:变体细分", value="V"),
@@ -145,6 +145,45 @@ async def mj_variation(ctx, number: int, change_sign: str = "U", reset_target : 
 
     await ctx.send("丁真正在画")
 
+
+# test
+@bot.command(
+    name="base_command",
+    description="This description isn't seen in UI (yet?)",
+    options=[
+        interactions.Option(
+            name="command_name",
+            description="A descriptive description",
+            type=interactions.OptionType.SUB_COMMAND,
+            options=[
+                interactions.Option(
+                    name="option",
+                    description="A descriptive description",
+                    type=interactions.OptionType.INTEGER,
+                    required=False,
+                ),
+            ],
+        ),
+        interactions.Option(
+            name="second_command",
+            description="A descriptive description",
+            type=interactions.OptionType.SUB_COMMAND,
+            options=[
+                interactions.Option(
+                    name="second_option",
+                    description="A descriptive description",
+                    type=interactions.OptionType.STRING,
+                    required=True,
+                ),
+            ],
+        ),
+    ],
+)
+async def cmd(ctx: interactions.CommandContext, sub_command: str, second_option: str = "", option: int = None):
+    if sub_command == "command_name":
+        await ctx.send(f"You selected the command_name sub command and put in {option}")
+    elif sub_command == "second_command":
+        await ctx.send(f"You selected the second_command sub command and put in {second_option}")
 
 
 bot.start()
