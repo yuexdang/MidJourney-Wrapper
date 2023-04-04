c688700675e7f04cf12edc55d29dc4d4ac17cdbf
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-04 23:43:20 +0800

    Update Globals.py

--- Globals.py
+++ Globals.py
@@ -31,15 +31,15 @@ userInfo       = {
 bot_name = "理塘丁真"
 
 update_msg = f"""
-DandjourneyV1.2 正式上线！
+DandjourneyV1.3 正式上线！
 Github链接：https://github.com/yuexdang/MidJourney-Wrapper
 目前挂载机器人：""" + bot_name + """
 最近更新时间：2023-04-03
 更新内容：
-1.dj指令拓展，支持6种自定义内容
-2.指令反馈，完善丁真反馈信息
-3.增加用户阻塞，在获取到丁真的注意后有30s时间进行操作
-4.狠狠的剥夺丁真的灵敏度，现在仅在回复MidJourney时会触发丁真
+1. 新增/dblend 、/fast 、/relax (整合为/speed 指令)  指令
+2. 更新/dj 垫图功能
+3. 修复身份串线等恶性bug
+4. 更新/usage 指令内容
 谨防盗版，支持白嫖
 """
 
@@ -47,9 +47,13 @@ help_info = f'''
         丁真珍珠目前支持图片的生成与细分，不想支持blend功能，info功能暂时也不打算集成上去
 
         指令：
-        /fuck 测试用的没什么用
-        /dj 等价/imagine
-        /xf 细分图片
+        /fuck (尾缀内容) -- 测试指令没啥用
+        /dj (图片参数) (图片尺寸) (Midjourney版本) (图片质量) (风格化) (图片种子) (图组差异化) (参考图片) (参考图片权重) -- 生成图片
+        /xf (所选图片索引) (操作方式) (机器人目标重置信号) -- 细分图片
+        /dblend (图片) (图片) (图片) (图片) (图片) (图片差异化) -- 融合图片
+        /speed (图片生成速度) -- 调整Midjourney出图速度
+        /info -- 关于机器人
+        /usage -- 机器人用法
 	
         如何进行细分：
         1.先找到你想要进行操作的Midjourney消息，并回复这条消息：丁真
