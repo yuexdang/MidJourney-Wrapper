8d709d58c6533657b68989e0be9227ebacc49a34
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-04-04 16:05:52 +0800

    Update Salai.py

--- Salai.py
+++ Salai.py
@@ -2,11 +2,11 @@ import Globals
 import requests
 
 def PassPromptToSelfBot(prompt : str):
-    payload ={"type":2,"application_id":"936929561302675456","guild_id":Globals.SERVER_ID,
+    payload ={"type":2,"application_id":Globals.MID_JOURNEY_ID,"guild_id":Globals.SERVER_ID,
               "channel_id":Globals.CHANNEL_ID,"session_id":"2fb980f65e5c9a77c96ca01f2c242cf6",
               "data":{"version":"1077969938624553050","id":"938956540159881230","name":"imagine","type":1,"options":[{"type":3,"name":"prompt","value":prompt}],
                       "application_command":{"id":"938956540159881230",
-                                             "application_id":"936929561302675456",
+                                             "application_id":Globals.MID_JOURNEY_ID,
                                              "version":"1077969938624553050",
                                              "default_permission":True,
                                              "default_member_permissions":None,
@@ -15,9 +15,9 @@ def PassPromptToSelfBot(prompt : str):
                                              "options":[{"type":3,"name":"prompt","description":"The prompt to imagine","required":True}]},
               "attachments":[]}}
     
-    {"type":2,"application_id":"936929561302675456",
+    {"type":2,"application_id":Globals.MID_JOURNEY_ID,
      "guild_id":"1091688748728733777","channel_id":"1091688749877969052","session_id":"a1dd523030181a4c27005dd743b50a28",
-     "data":{"version":"1077969938624553050","id":"938956540159881230","name":"imagine","type":1,"options":[{"type":3,"name":"prompt","value":"girl"}],"application_command":{"id":"938956540159881230","application_id":"936929561302675456","version":"1077969938624553050","default_permission":True,"default_member_permissions":None,"type":1,"nsfw":False,"name":"imagine","description":"Create images with Midjourney","dm_permission":True,"options":[{"type":3,"name":"prompt","description":"The prompt to imagine","required":True}]},"attachments":[]},"nonce":"1092673769505292288"}
+     "data":{"version":"1077969938624553050","id":"938956540159881230","name":"imagine","type":1,"options":[{"type":3,"name":"prompt","value":"girl"}],"application_command":{"id":"938956540159881230","application_id":Globals.MID_JOURNEY_ID,"version":"1077969938624553050","default_permission":True,"default_member_permissions":None,"type":1,"nsfw":False,"name":"imagine","description":"Create images with Midjourney","dm_permission":True,"options":[{"type":3,"name":"prompt","description":"The prompt to imagine","required":True}]},"attachments":[]},"nonce":"1092673769505292288"}
 
     header = {
         'authorization' : Globals.SALAI_TOKEN
@@ -32,7 +32,7 @@ def Upscale(index : int, messageId : str, messageHash : str):
     "channel_id":Globals.CHANNEL_ID,
     "message_flags":0,
     "message_id": messageId,
-    "application_id":"936929561302675456",
+    "application_id":Globals.MID_JOURNEY_ID,
     "session_id":"45bc04dd4da37141a5f73dfbfaf5bdcf",
     "data":{"component_type":2,
             "custom_id":"MJ::JOB::upsample::{}::{}".format(index, messageHash)}
@@ -53,7 +53,7 @@ def MaxUpscale(messageId : str, messageHash : str):
             "channel_id":Globals.CHANNEL_ID,
                "message_flags":0,
                "message_id": messageId,
-               "application_id":"936929561302675456",
+               "application_id":Globals.MID_JOURNEY_ID,
                "session_id":"1f3dbdf09efdf93d81a3a6420882c92c","data": 
          {"component_type":2,"custom_id":"MJ::JOB::upsample_max::1::{}::SOLO".format(messageHash)}}
     header = {
@@ -69,7 +69,7 @@ def Variation(index : int,messageId : str, messageHash : str):
                 "channel_id": Globals.CHANNEL_ID,
                 "message_flags":0,
                 "message_id": messageId,
-                "application_id": "936929561302675456",
+                "application_id": Globals.MID_JOURNEY_ID,
                 "session_id":"1f3dbdf09efdf93d81a3a6420882c92c",
                 "data":{"component_type":2,"custom_id":"MJ::JOB::variation::{}::{}".format(index, messageHash)}}
     header = {
@@ -91,14 +91,14 @@ def BlendImg(image: list, dimensions: str):
     options.append({"type":3,"name":"dimensions","value":dimensions})    
     
     payload = {
-        "type":2,"application_id":"936929561302675456",
+        "type":2,"application_id":Globals.MID_JOURNEY_ID,
         "guild_id":Globals.SERVER_ID,"channel_id":Globals.CHANNEL_ID,
-        "session_id":"c0d7cc878d2433f0a5633bab61a1eb77",
+        "session_id":"5da55e5780df4df1f4775c464245cba1",
         "data":{
             "version":"1067631020041580584","id":"1062880104792997970",
             "name":"blend","type":1,"options":options,
                 "application_command":{
-                    "id":"1062880104792997970","application_id":"936929561302675456",
+                    "id":"1062880104792997970","application_id":Globals.MID_JOURNEY_ID,
                     "version":"1067631020041580584",
                     "default_permission":True,
                     "default_member_permissions":None,"type":1,"nsfw":False,
